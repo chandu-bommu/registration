cc24ccaca3d5e58ccb2d0d1f662b0ed34d1f84a3
Prathap, Chandrasekhar <Chandrasekhar_Prathap@intuit.com>
2018-02-15 19:24:44 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld.jsx'
 ReactDOM.render(<HelloWorld />, document.getElementById('app'));
